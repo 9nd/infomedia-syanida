5fb725b9324b62fb99ba8b2f5842c3bda363513a
mochrafly04 <mochrafly04@gmail.com>
2022-07-26 10:37:59 +0700

    2

--- application/controllers/Kelola_dokumen/Kelola.php
+++ application/controllers/Kelola_dokumen/Kelola.php
@@ -7,7 +7,7 @@ class Kelola extends CI_Controller
 	public function __construct()
 	{
 		parent::__construct();
-		$this->load->model('M_kelola_dokumen');
+		$this->load->model('Kelola dokumen/M_kelola_dokumen');
 		$this->load->library('form_validation');
 		$this->load->helper(array('url', 'download'));
 	}
